c535aa4baf837d9a4fa2ffcb1a947dbd4ca9a83f
ub <karges@ub.local>
2011-07-17 22:30:50 -0400

    blah 4

--- db/migrate/20110717225943_add_priority_column_again.rb
+++
@@ -1,9 +0,0 @@
-class AddPriorityColumnAgain < ActiveRecord::Migration
-  def self.up
-    add_column :tasks, :priority, :integer
-  end
-
-  def self.down
-    remove_column :tasks, :priority
-  end
-end
