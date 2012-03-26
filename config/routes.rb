488b93ed15dfc3e7cf201a4e9527ac2959ee4b31
ub <karges@ub.local>
2012-03-25 20:31:31 -0400

    changed home to

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Activity::Application.routes.draw do
   
   devise_for :users
 
-  root :to => "pages#index"
+  root :to => "tasks#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
